04c9103e99cb97b2ec4b0c249e88f21aeb25418f
Amir Kozhanov <amirkozanov@gmail.com>
2023-04-02 14:26:58 +0600

    first load

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    autoplay: {
+        delay: 3000,
+    }
+});
+
+const mobile = document.querySelector('.header__mobile')
+const aside = document.querySelector('.mobile')
+const exit = document.querySelector('.mobile_exit')
+
+mobile.addEventListener('click', () => {
+    aside.classList.add('mobile_active')
+})
+
+exit.addEventListener('click', () => {
+    aside.classList.remove('mobile_active')
+})
